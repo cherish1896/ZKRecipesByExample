e9d7c7ca980ecdb794b307525697c9fb9ca20657
cherish1896 <cherish1896@126.com>
2017-01-10 17:57:56 +0800

    add log

--- src/main/java/com/colobu/zkrecipe/leaderelection/ExampleClient.java
+++ src/main/java/com/colobu/zkrecipe/leaderelection/ExampleClient.java
@@ -3,6 +3,9 @@ package com.colobu.zkrecipe.leaderelection;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
 import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -10,37 +13,39 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ExampleClient extends LeaderSelectorListenerAdapter implements Closeable {
-	private final String name;
-	private final LeaderSelector leaderSelector;
-	private final AtomicInteger leaderCount = new AtomicInteger();
 
-	public ExampleClient(CuratorFramework client, String path, String name) {
-		this.name = name;
-		leaderSelector = new LeaderSelector(client, path, this);
-		leaderSelector.autoRequeue();
-	}
+    private static Logger logger = LoggerFactory.getLogger(ExampleClient.class);
+    private final String name;
+    private final LeaderSelector leaderSelector;
+    private final AtomicInteger leaderCount = new AtomicInteger();
 
-	public void start() throws IOException {
-		leaderSelector.start();
-	}
+    public ExampleClient(CuratorFramework client, String path, String name) {
+        this.name = name;
+        leaderSelector = new LeaderSelector(client, path, this);
+        leaderSelector.autoRequeue();
+    }
 
-	@Override
-	public void close() throws IOException {
-		leaderSelector.close();
-	}
-	
-	@Override
-	public void takeLeadership(CuratorFramework client) throws Exception {
-		final int waitSeconds = (int) (5 * Math.random()) + 1;
-		System.out.println(name + " is now the leader. Waiting " + waitSeconds + " seconds...");
-		System.out.println(name + " has been leader " + leaderCount.getAndIncrement() + " time(s) before.");
-		try {
-			Thread.sleep(TimeUnit.SECONDS.toMillis(waitSeconds));
-		} catch (InterruptedException e) {
-			System.err.println(name + " was interrupted.");
-			Thread.currentThread().interrupt();
-		} finally {
-			System.out.println(name + " relinquishing leadership.\n");
-		}
-	}
+    public void start() throws IOException {
+        leaderSelector.start();
+    }
+
+    @Override
+    public void close() throws IOException {
+        leaderSelector.close();
+    }
+
+    @Override
+    public void takeLeadership(CuratorFramework client) throws Exception {
+        final int waitSeconds = (int) (5 * Math.random()) + 1;
+        logger.info("{} is now the leader. Waiting {} seconds...", this.name, waitSeconds);
+        logger.info("{} has been leader {} time(s) before.", this.name, leaderCount.getAndIncrement());
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(waitSeconds));
+        } catch (InterruptedException e) {
+            logger.error(name + " was interrupted.", e);
+            Thread.currentThread().interrupt();
+        } finally {
+            logger.info(name + " relinquishing leadership now.\n");
+        }
+    }
 }
